2cea4630b7ded5d0f92a380dc6e5119a164e5efa
Nicola Ferralis <feranick@hotmail.com>
2017-03-25 22:27:16 -0400

    Rewritten webserver.

--- Other/WebServer/button.php
+++ Other/WebServer/button.php
@@ -1,56 +1,32 @@
 <?php
 $file = "buttonStatus.txt";
 $handle = fopen($file,'w+');
-if (isset($_POST['on']))
-{
-$onstring = "ON";
-fwrite($handle,$onstring);
+if (isset($_POST['up']))
+  { $status = "UP"; }
+else if(isset($_POST['down']))
+  { $status = "DOWN";}
+else if(isset($_POST['left']))
+  { $status = "LEFT";}
+else if(isset($_POST['right']))
+  { $status = "RIGHT";}
+
+fwrite($handle,$status);
 fclose($handle);
+
 print "
 <html>
 <body>
-<title>DIY Hacking</title>
 <style type=text/css>
-h1{
-	padding-left: 300px;
-}
 h2{
-	position: absolute;
-	top: 100px;
-	left: 450px;
+        position: absolute;
+        top: 75px;
+        left: 225px;
 }
 </style>
-<h1>DIY Hacking - Internet of Things Implementation</h2>
-<h2>The Device has been Turned ON </h2>
+<h2>Going $status</h2>
 </body>
 </html>
 ";
-}
-else if(isset($_POST['off']))
-{
-$offstring = "OFF";
-fwrite($handle, $offstring);
-fclose($handle);
-print "
-<html>
-<body>
-<title>DIY Hacking</title>
-<style type=text/css>
-h1{
-	padding-left: 300px;
-}
-h2{
-	position: absolute;
-	top: 100px;
-	left: 450px;
-}
-</style>
-<h1>DIY Hacking - Internet of Things Implementation</h2>
-<h2>The Device has been Turned OFF </h2>
-</body>
-</html>
-";
-}
 
-
+include('index.html');
 ?>
