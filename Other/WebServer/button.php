bdce9af9850d6da4d3d01117e8639238afc1f52e
Nicola Ferralis <feranick@hotmail.com>
2017-03-27 12:52:02 -0400

    Split steering and power control.

--- Other/WebServer/button.php
+++ Other/WebServer/button.php
@@ -1,26 +1,30 @@
 <?php
-$file = "buttonStatus.txt";
-$handle = fopen($file,'w+');
-if (isset($_POST['up']))
-  { $status = "UP"; }
-else if(isset($_POST['down']))
-  { $status = "DOWN";}
-else if(isset($_POST['left']))
-  { $status = "LEFT";}
-else if(isset($_POST['right']))
-  { $status = "RIGHT";}
+$powerFile = "powerStatus.txt";
+$steerFile = "steerStatus.txt";
 
+if (isset($_POST['steer']))
+{	$file = $steerFile;
+	$status = $_POST['steer'];}
+else
+{	$file = $powerFile;
+	$status = $_POST['power'];}
+
+$handle = fopen($file,'w');
 fwrite($handle,$status);
 fclose($handle);
 
 print "
+<script>
+console.log('PHP:" .$status. "');
+</script>
+
 <html>
 <body>
 <style type=text/css>
 h2{
         position: absolute;
-        top: 75px;
-        left: 225px;
+        top: 150px;
+        left: 100px;
 }
 </style>
 <h2>Going $status</h2>
--- Other/WebServer/rasbpi.py
+++ Other/WebServer/rasbpi.py
@@ -9,20 +9,34 @@ GPIO.setmode(GPIO.BOARD)
 GPIO.setup(5,GPIO.OUT)
 true = 1
 
-fileName = "buttonStatus.txt"
+steerFile = "steerStatus.txt"
+powerFile = "powerStatus.txt"
 
 while(true):
-    with open(fileName, 'r') as f:
-        status = f.readlines()
-    if(status != 'ZERO'):
+    with open(powerFile, 'r') as f:
+        powerStatus = f.readlines()
+    print(powerStatus)
+    with open(steerFile, 'r') as f:
+        steerStatus = f.readlines()
+    print(steerStatus)
+    speep(0.2)
+    if(steerStatus != 'ZERO'):
         with open(fileName, 'ab') as f:
             f.write("ZERO")
-    sleep(0.2)
-    print(status)
+    	sleep(0.2)
+    	print(steerStatus)
 
     # This needs fixing with proper channels
-    if status=='UP':
+    if powerStatus=='UP':
         GPIO.output(5,True)
-    elif status=='DOWN':
+    elif powerStatus=='DOWN':
+        GPIO.output(5,False)
+    elif powerStatus=='STOP'
         GPIO.output(5,False)
 
+    if steerStatus=='LEFT':
+        GPIO.output(5,True)
+    elif steerStatus=='RIGHT':
+        GPIO.output(5,False)
+    elif steerStatus=='ZERO'
+        GPIO.output(5,False)
