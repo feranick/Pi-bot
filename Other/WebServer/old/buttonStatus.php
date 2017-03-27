40a1da62c020ab9fbf53b14cb3b0547ec14e289d
Nicola Ferralis <feranick@hotmail.com>
2017-03-27 16:42:35 -0400

    Folder cleanup

--- Other/WebServer/old/buttonStatus.php
+++
@@ -1,4 +0,0 @@
-<?php
-$status = file_get_contents("buttonStatus.txt");
-echo $status;
-?>
--- Other/WebServer/old/reset_position.php
+++
@@ -1,8 +0,0 @@
-<?php
-$file = "buttonStatus.txt";
-$handle = fopen($file,'w+');
-$status = "ZERO";
-fwrite($handle,$status);
-fclose($handle);
-echo $status
-?>
