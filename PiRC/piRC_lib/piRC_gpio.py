761bf4094cab3bd458ca1f3af316729054bf7d3b
Nicola Ferralis <feranick@hotmail.com>
2017-03-29 15:12:02 -0400

    Updated GPIO assignments.

--- PiRC/piRC_lib/piRC_gpio.py
+++ PiRC/piRC_lib/piRC_gpio.py
@@ -4,7 +4,7 @@
 **********************************************************
 *
 * PiRC_gpio library
-* version: 20170328b
+* version: 20170329a
 *
 * By: Nicola Ferralis <feranick@hotmail.com>
 *
@@ -13,16 +13,26 @@
 
 import RPi.GPIO as GPIO
 
+IR1 = 3
+IR2 = 5
+
+AIN1 = 11
+AIN2 = 13
+PWNA = 15
+BIN1 = 19
+BIN2 = 21
+PWNB = 23
+
 GPIO.setwarnings(False)
 GPIO.setmode(GPIO.BOARD)
-GPIO.setup(3, GPIO.IN)                            #Right sensor connection
-GPIO.setup(16, GPIO.IN, pull_up_down=GPIO.PUD_UP) #Left sensor connection
+GPIO.setup(IR1, GPIO.IN)                            #Right sensor connection
+GPIO.setup(IR2, GPIO.IN, pull_up_down=GPIO.PUD_UP) #Left sensor connection
 
 #these need to be fixed for TB6612
-GPIO.setup(5,GPIO.OUT)   #AIN1 motor input A
-GPIO.setup(7,GPIO.OUT)   #AIN2 motor input B
-#GPIO.setup(5,GPIO.OUT)   #PWNA motor input B (power) - analog
-GPIO.setup(11,GPIO.OUT)  #BIN1 motor input A
-GPIO.setup(13,GPIO.OUT)  #BIN2 motor input B
-#GPIO.setup(14,GPIO.OUT)   #PWNA motor input B (power) - analog
+GPIO.setup(AIN1,GPIO.OUT)   #AIN1 motor input A
+GPIO.setup(AIN2,GPIO.OUT)   #AIN2 motor input B
+GPIO.setup(PWNA,GPIO.OUT)   #PWNA motor input B (power) - analog
+GPIO.setup(BIN1,GPIO.OUT)  #BIN1 motor input A
+GPIO.setup(BIN2,GPIO.OUT)  #BIN2 motor input B
+GPIO.setup(PWNB,GPIO.OUT)   #PWNA motor input B (power) - analog
 
--- PiRC/piRC_lib/piRC_lib.py
+++ PiRC/piRC_lib/piRC_lib.py
@@ -4,7 +4,7 @@
 **********************************************************
 *
 * PiRC_lib
-* version: 20170328b
+* version: 20170329b
 *
 * By: Nicola Ferralis <feranick@hotmail.com>
 *
@@ -16,20 +16,19 @@ from time import sleep
 import sys
 import random as rd
 
-
 #************************************
 ''' Control Motors'''
 #************************************
 def runMotor(motor, state):
     if motor == 1:  # motor for powering vehicle
-        in1 = 5
-        in2 = 7
-        #pwn = 3
+        in1 = AIN1
+        in2 = AIN2
+        pwn = PWNA
     
     elif motor == 0:    # motor for steering
-        in1 = 11
-        in2 = 13
-        #pwn = 6
+        in1 = BIN1
+        in2 = BIN2
+        pwn = PWNB
     
     if state == -1:
         GPIO.output(in1,0)
@@ -41,14 +40,14 @@ def runMotor(motor, state):
         GPIO.output(in1,1)
         GPIO.output(in2,0)
     #full power
-    #GPIO.output(pwn, 255)
+    GPIO.output(pwn, 255)
 
 #************************************
 ''' Read IR sensors '''
 #************************************
 def irSensors():
-    l=GPIO.input(3)                         #Reading output of right IR sensor
-    r=GPIO.input(16)                        #Reading output of left IR sensor
+    l=GPIO.input(IR1)                         #Reading output of right IR sensor
+    r=GPIO.input(IR2)                        #Reading output of left IR sensor
     return l, r
 
 #************************************
