5b77ac60f11307228e8cb2658014bed059ab0ca8
Nicola Ferralis <feranick@hotmail.com>
2017-04-25 13:33:31 -0400

    Reordered parameters in ML training file.

--- piRC/piRC_ML/piRC_write_learning.py
+++ piRC/piRC_ML/piRC_write_learning.py
@@ -31,17 +31,17 @@ def main():
     #make sure motors are stopped
     fullStop()
     
-    #filename = 'Training_lrcbxyzsp' + str(datetime.now().strftime('_%Y-%m-%d_%H-%M-%S.txt'))
-    filename = 'Training_lrcbxyzsp.txt'
+    #filename = 'Training_splrcbxyz' + str(datetime.now().strftime('_%Y-%m-%d_%H-%M-%S.txt'))
+    filename = 'Training_splrcbxyz.txt'
 
     while True:
         try:
             l,r,c,b = readAllSonars(TRIG, ECHO)
             x,y,z = readAccel(True)
             s, p = statMotors()
-            print(' L={0:.2f}, R={1:.2f}, C={2:.2f}, B={3:.2f}, X={4:.2f}, Y={5:.2f}, Z={6:.2f}, S={7:.1f}, P={7:.1f}'.format(l,r,c,b,x,y,z,s,p))
+            print(' L={0:.1f}, R={1:.1f}, C={2:.2f}, B={3:.2f}, X={4:.2f}, Y={5:.2f}, Z={6:.2f}, S={7:.2f}, P={7:.2f}'.format(s,p,l,r,c,b,x,y,z))
             with open(filename, "a") as sum_file:
-                sum_file.write('{0:.2f},{1:.2f},{2:.2f},{3:.2f},{4:.2f},{5:.2f},{6:.2f},{7:.1f},{8:.1f}\n'.format(l,r,c,b,x,y,z,s,p))
+                sum_file.write('{0:.1f},{1:.1f},{2:.2f},{3:.2f},{4:.2f},{5:.2f},{6:.2f},{7:.2f},{8:.2f}\n'.format(s,p,l,r,c,b,x,y,z))
         except:
             #fullStop()
             GPIO.cleanup()
