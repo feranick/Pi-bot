11ec48a0d6787f02c25279c6f7b546b1a2c2b2f8
Nicola Ferralis <feranick@hotmail.com>
2017-04-24 23:22:55 -0400

    Improvements on writing learning file.

--- PiRC/piRC_write_learning.py
+++ PiRC/piRC_write_learning.py
@@ -31,16 +31,17 @@ def main():
     #make sure motors are stopped
     fullStop()
     
-    filename = 'Training_lrcbxyzsp_' + str(datetime.now().strftime('_%Y-%m-%d_%H-%M-%S.txt'))
-    
+    #filename = 'Training_lrcbxyzsp' + str(datetime.now().strftime('_%Y-%m-%d_%H-%M-%S.txt'))
+    filename = 'Training_lrcbxyzsp.txt'
+
     while True:
         try:
             l,r,c,b = readAllSonars(TRIG, ECHO)
             x,y,z = readAccel(True)
             s, p = statMotors()
-            #print(' L={0}, R={1}, C={2}, B={3}, X={4}, Y={5}, Z={6}'.format(l,r,c,b,x,y,z,s,p))
+            print(' L={0:.2f}, R={1:.2f}, C={2:.2f}, B={3:.2f}, X={4:.2f}, Y={5:.2f}, Z={6:.2f}, S={7:.1f}, P={7:.1f}'.format(l,r,c,b,x,y,z,s,p))
             with open(filename, "a") as sum_file:
-                sum_file.write('{0:.3f},{1:.2f},{2:.2f},{3:.2f},{4:.2f},{5:.2f},{6:.2f},{7:.2f},{8:.2f}\n'.format(l,r,c,b,x,y,z,s,p))
+                sum_file.write('{0:.2f},{1:.2f},{2:.2f},{3:.2f},{4:.2f},{5:.2f},{6:.2f},{7:.1f},{8:.1f}\n'.format(l,r,c,b,x,y,z,s,p))
         except:
             #fullStop()
             GPIO.cleanup()
