987b1538bb3b2078881e6e602f5d1f69488bf02c
Nicola Ferralis <feranick@hotmail.com>
2017-04-26 20:43:09 -0400

    Reduced precision in distance detection.

--- piRC/piRC_ML/piRC_write_training.py
+++ piRC/piRC_ML/piRC_write_training.py
@@ -4,7 +4,7 @@
 **********************************************************
 *
 * PiRC - write training file from sensors
-* version: 20170426a
+* version: 20170426c
 *
 * By: Nicola Ferralis <feranick@hotmail.com>
 *
@@ -39,9 +39,9 @@ def main():
             l,r,c,b = readAllSonars(TRIG, ECHO)
             x,y,z = readAccel(True)
             s,p = statMotors()
-            print(' S={0:.0f}, P={1:.0f}, L={2:.2f}, R={3:.2f}, C={4:.2f}, B={5:.2f}, X={6:.3f}, Y={7:.3f}, Z={8:.3f}'.format(s,p,l,r,c,b,x,y,z))
+            print(' S={0:.0f}, P={1:.0f}, L={2:.0f}, R={3:.0f}, C={4:.0f}, B={5:.0f}, X={6:.3f}, Y={7:.3f}, Z={8:.3f}'.format(s,p,l,r,c,b,x,y,z))
             with open(filename, "a") as sum_file:
-                sum_file.write('{0:.0f}\t{1:.0f}\t{2:.2f}\t{3:.2f}\t{4:.2f}\t{5:.2f}\t{6:.3f}\t{7:.3f}\t{8:.3f}\n'.format(s,p,l,r,c,b,x,y,z))
+                sum_file.write('{0:.0f}\t{1:.0f}\t{2:.0f}\t{3:.0f}\t{4:.0f}\t{5:.0f}\t{6:.3f}\t{7:.3f}\t{8:.3f}\n'.format(s,p,l,r,c,b,x,y,z))
         except:
             fullStop()
             GPIO.cleanup()
