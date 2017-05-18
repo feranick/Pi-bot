325dbdb0730dd2387f94702f9585b5fdd0b9f1f9
Nicola Ferralis <feranick@hotmail.com>
2017-05-18 16:38:49 -0400

    Improved interface of syncTFile.sh

--- piRC/machine_learning/syncTraining/syncTFile.sh
+++ piRC/machine_learning/syncTraining/syncTFile.sh
@@ -9,9 +9,14 @@ else
    trainFile=$1
 fi
 
+echo
+echo "Transferring training file (" $trainFile.txt ") to:" $server
 scp -C $trainFile.txt $server:$folder
 
-echo "Running trainning..."
-ssh -C $server $folder/piRC_ML.py -t $folder/$trainFile.txt 
+echo
+echo "Running training using:" $trainFile.txt
+ssh -C $server $folder/piRC_ML.py -t $folder/$trainFile.txt
 
+echo "Transferring training model (" $trainFile.nnModelC.pkl ") from:" $server
 scp -C $server:$folder/$trainFile.nnModelC.pkl .
+echo
