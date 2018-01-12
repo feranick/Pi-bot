6462ab5733d6a3f183925e93fccbda29be3d0e79
Nicola Ferralis <feranick@hotmail.com>
2018-01-12 14:41:28 -0500

    Bug fixes

--- Utilities/camera/no_file/snapshot2.py
+++ Utilities/camera/no_file/snapshot2.py
@@ -12,20 +12,20 @@ def get_image():
     camera.hflip = True
     output = np.empty((size[0],size[1],3), dtype=np.uint8)
     camera.capture(output, 'rgb')
+    camera.close()
     print(output.shape)
     img = Image.fromarray(output).convert('L')
     img_resized = img.resize(resized_size, Image.ANTIALIAS)
     data = np.asarray( img_resized, dtype="int32" )
     print(data.shape)
-    np.savetxt('test.out', data, fmt='%d')
+    #np.savetxt('test.out', data, fmt='%d')
     return data
 
 def save_image( npdata, outfilename ) :
     img = Image.fromarray( np.asarray( np.clip(npdata,0,255), dtype="uint8"), "L" )
     img.save( outfilename )
 
-
-data = get_image()
-
-print(data)
+while True:
+    data = get_image()
+    print(data)
 
